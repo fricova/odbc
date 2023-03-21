8b00510f8172aea840163882be8954b89056ae85
Michaela Fricova <92326639+misacodes@users.noreply.github.com>
2023-03-21 11:04:59 +0100

    Update odbc_python.py

--- odbc_python.py
+++ odbc_python.py
@@ -2,11 +2,11 @@ import pyodbc
 import pandas as pd
 
 connection = pyodbc.connect('Driver={SQL Server};'
-                      'Server=DB01;'
-                      'VINONLINE;'
+                      'Server=Server;'
+                      'Database;'
                       'Trusted_Connection=yes;')
 
 cursor = connection.cursor()
-query = cursor.execute('SELECT * FROM [VINONLINE].[dbo].[Vehicle]')
+query = cursor.execute('SELECT * FROM [Server].[dbo].[Database]')
 table = pd.DataFrame(query)
 print(table)
