10d1db2d04105705af999dec49ca184dd2a34a71
Michaela Fricova <92326639+misacodes@users.noreply.github.com>
2023-03-21 11:04:28 +0100

    Update odbc_rstudio.R

--- odbc_rstudio.R
+++ odbc_rstudio.R
@@ -4,11 +4,11 @@ con <- dbConnect(odbc::odbc(), "SQLserveR")
 {sql connection=con, output.var = data}
 ```{sql connection=con, output.var = data}
 select TOP (1000) *
-from Autotracer.dbo.at_dotaz
+from database.dbo.dataset
 ```
 
 data <- 
   dbGetQuery(con,
              'select TOP (1000) *
-from Autotracer.dbo.at_dotaz')
+from database.dbo.dataset')
 data
